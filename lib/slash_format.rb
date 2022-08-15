0ee750b45af4b52c0f155eb3b3d3c181c668fcc9
Spotlight <spotlight@joscomputing.space>
2022-08-15 15:32:56 -0500

    Add initial slash invocation handling

--- lib/slash_format.rb
+++ lib/slash_format.rb
@@ -25,9 +25,6 @@ class SlashFormat
     command_format = command[:arg_format]
 
     @bot.register_application_command(command_name, command_description) do |options|
-      # Not all slash commands require a format.
-      next if command_format.nil?
-
       map_arg_format_to_options command_format, options
     end
 
@@ -35,7 +32,10 @@ class SlashFormat
 
     @bot.application_command(command_name) do |event|
       # TODO: handle
-      puts "Ran command #{event.command_name}"
+      puts "Ran slash command #{event.command_name}" if @command_bot.debug_mode
+
+      args = derive_arguments command_format, event
+      command[:code].call(event, args)
     end
   end
 
@@ -47,6 +47,9 @@ class SlashFormat
   # @!macro arg_format
   # @param [Discordrb::Interactions::OptionBuilder] options
   def map_arg_format_to_options(arg_format, options)
+    # Not all slash commands require a format.
+    return nil if arg_format.nil?
+
     arg_format.each do |symbol, format|
       name = symbol.to_s
       description = format[:description]
@@ -83,6 +86,80 @@ class SlashFormat
       else
         format_error 'Unimplemented type given!'
       end
+    end
+  end
+
+  # Maps our custom arg_format to the official builder.
+  # @!macro arg_format
+  # @param event [Discordrb::Events::ApplicationCommandEvent] The current event.
+  # @return [ArgumentHash{Symbol => Object}] A hash mapping an argument's symbol to its value.
+  def derive_arguments(arg_format, event)
+    # Not all slash commands require a format.
+    return nil if arg_format.nil?
+
+    options = event.options
+    # Return an ArgumentHash for dot-notation access.
+    result_args = ArgumentHash.new
+
+    # The format name is the name specified within arg_format itself,
+    # and is what we use to identify an argument's value.
+    arg_format.each do |format_name, format|
+      # The Discord name is the name given to Discord to identify an option,
+      # and is what they use to identify an argument's value.
+      # This is needed to look up result values, and resolved types.
+      discord_name = format[:name]
+      arg_type = format[:type]
+
+      # Ensure Discord gave us this parameter.
+      discord_arg_value = options[discord_name]
+
+      if discord_arg_value.nil?
+        # If we aren't optional, this should not be nil.
+        # TODO: properly handle error
+        raise NotEnoughArgumentsError unless format[:optional]
+
+        # Otherwise, ensure we set the default.
+        result_args[format_name] = determine_default format, event
+        next
+      end
+
+      # Switch by argument type.
+      arg_value = case arg_type
+                  when :string, :remaining, :integer, :number, :boolean
+                    discord_arg_value
+                  when :user
+                    event.resolved.users[discord_name]
+                  when :channel
+                    event.resolved.channels[discord_name]
+                  else
+                    format_error 'Unimplemented type given!'
+                  end
+      result_args[format_name] = arg_value
+    end
+
+    result_args
+  end
+
+  # Determines the default value for the current format.
+  # TODO: This is a derivation of text_format's determine_default -
+  # we should likely consolidate them.
+  # @param format The current arg_format.
+  # @param event [ApplicationCommandEventHandler] The current event.
+  # @return [Object] Default value specified for the format.
+  def determine_default(format, event)
+    return nil unless format.key? :default
+
+    # Users and channels can have symbols defaults, allowing context-aware defaults.
+    default = format[:default]
+    type = format[:type]
+
+    if type == :user && default == :current_user
+      event.user
+    elsif type == :channel && default == :current_channel
+      event.channel
+    else
+      # Use the provided default per the format.
+      default
     end
   end
 end
